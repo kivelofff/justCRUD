10dcab69ace9818851583d6f8e632609a14129ab
ladmin <kivelofff@gmail.com>
2020-09-07 23:29:59 +0200

    Initial commit

---
+++ src/main/java/org/example/controller/CustomerController.java
@@ -0,0 +1,21 @@
+package org.example.controller;
+
+import org.example.model.Customer;
+import org.example.model.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+    @RequestMapping("/")
+    public ModelAndView home() {
+        List<Customer> listCustomer = customerService.listAll();
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("listCustomer", listCustomer);
+        return mav;
+    }
+}
